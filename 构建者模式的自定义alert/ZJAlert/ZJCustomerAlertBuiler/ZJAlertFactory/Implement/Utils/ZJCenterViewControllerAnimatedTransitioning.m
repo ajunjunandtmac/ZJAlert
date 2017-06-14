850000383b5da20c27a76b0d2e905e1724644d9e
ajunjunandtmac <ajunjunandtmac@163.com>
2017-06-14 20:17:58 +0800

    添加alert转场动画

---
+++ 构建者模式的自定义alert/ZJAlert/ZJCustomerAlertBuiler/ZJAlertFactory/Implement/Utils/ZJCenterViewControllerAnimatedTransitioning.m
@@ -0,0 +1,13 @@
+//
+//  ZJCenterViewControllerAnimatedTransitioning.m
+//  构建者模式的自定义alert
+//
+//  Created by jiale on 2017/6/14.
+//  Copyright © 2017年 jiale. All rights reserved.
+//
+
+#import "ZJCenterViewControllerAnimatedTransitioning.h"
+
+@implementation ZJCenterViewControllerAnimatedTransitioning
+
+@end
